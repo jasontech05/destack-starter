930e2c2b9adbb401d35ccf5321d6f502a6e91863
JasonTech05 <jasonabdi@gmail.com>
2024-07-01 14:44:51 -0600

    "Added import statements and a new default export function Page, removed default export of ContentProvider"

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,12 @@
+import { ContentProviderGrapes } from 'destack'
+import 'grapesjs/dist/css/grapes.min.css'
+
 export { getStaticProps } from 'destack/build/server'
-export { ContentProvider as default } from 'destack'
+
+export default function Page(props) { 
+    return (
+        <div style={{height: '100%'}}>
+            <span>Hello world</span>
+            <ContentProviderGrapes {...props}/>
+        </div>)
+}
